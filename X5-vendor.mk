75b48ac2042fcf881d94deadfa92b74691927785
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-10-15 19:25:48 +0200

    X5: Inherrit vendor blobs from a correct path

--- X5-vendor.mk
+++ X5-vendor.mk
@@ -1,10 +1,9 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/proprietary/X5/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/X5/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/proprietary/X5/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/X5/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/proprietary/X5/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/X5/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/proprietary/X5/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/X5/proprietary/vendor,system/vendor)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/proprietary/X5/xbin,system/xbin)
-
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/X5/proprietary/xbin,system/xbin)
