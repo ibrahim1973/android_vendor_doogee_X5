006d24b398adc8573c4b7ef91f33a3d9a6e9dfb4
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-10-15 18:01:02 +0200

    Initial commit

---
+++ X5-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/proprietary/X5/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/proprietary/X5/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/proprietary/X5/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/proprietary/X5/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/proprietary/X5/xbin,system/xbin)
+
